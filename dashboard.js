d0e5b093e215e10cc7bdfc0be1e5860497f09c01
hernanbosqued <hernanbosqued@gmail.com>
2019-03-08 12:48:09 -0300

    sixth

--- dashboard.js
+++ dashboard.js
@@ -1,15 +1,19 @@
 const { RTMClient, WebClient } = require('@slack/client');
-const token = process.env.SLACK_TOKEN;
+//const token = process.env.SLACK_TOKEN;
+const token = 'xoxb-58530797379-565023860738-8zlD6gWqcA9Xo6G0x6iiOHrJ';
+
 const rtm = new RTMClient(token);
 const web = new WebClient(token);
 
 rtm.start();
 
+console.log(`service up`);
+
 rtm.on('message', (message) => {
 
-  if ( (message.subtype && message.subtype === 'bot_message') || (!message.subtype && message.user === rtm.activeUserId) ) {
-    return;
-  }
+//  if ( (message.subtype && message.subtype === 'bot_message') || (!message.subtype && message.user === rtm.activeUserId) ) {
+//    return;
+//  }
 
   console.log(`(channel:${message.channel}) ${message.user} says: ${message.text}`);
   
@@ -26,6 +30,12 @@ rtm.on('message', (message) => {
 			var channel = res.channel.name;
 		  }
 		  
+		  rtm.sendMessage('Gracias por tu mensaje ' + user + '!', message.channel)
+		  .then((res) => {
+		    console.log('Message sent');
+		  })
+		  
+	  
 		  console.log(`(channel:${channel}) ${user} says: ${message.text}`);		  
 	})();
 });
