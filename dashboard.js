798c486d912ea82d3f734c846f337e968531f972
hernanbosqued <hernanbosqued@gmail.com>
2019-03-08 09:26:10 -0300

    first commit

---
+++ dashboard.js
@@ -0,0 +1,31 @@
+const { RTMClient, WebClient } = require('@slack/client');
+const token = "xoxb-58530797379-565023860738-otyqM5iyA8jJPphpFILdo8E4";
+const rtm = new RTMClient(token);
+const web = new WebClient(token);
+
+rtm.start();
+
+rtm.on('message', (message) => {
+
+  if ( (message.subtype && message.subtype === 'bot_message') || (!message.subtype && message.user === rtm.activeUserId) ) {
+    return;
+  }
+
+  console.log(`(channel:${message.channel}) ${message.user} says: ${message.text}`);
+  
+	(async () => {		
+		  var res = await web.users.info({user: message.user});
+		  if(res.ok){
+			  console.log('Nombre de usuario -> ' + res.user.real_name);
+			  var user = res.user.real_name;
+		  }
+	  
+		  res = await web.channels.info({channel: message.channel});
+		  if( res.ok ){
+			console.log('Nombre del canal -> ' + res.channel.name);
+			var channel = res.channel.name;
+		  }
+		  
+		  console.log(`(channel:${channel}) ${user} says: ${message.text}`);		  
+	})();
+});
